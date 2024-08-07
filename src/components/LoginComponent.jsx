5eeace031e34bd963a58212079ebe9b08fd49f41
animesh <sethanimesh@hotmail.com>
2024-08-07 10:45:28 +0530

    adding effects using javascript in login #1

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import $ from 'jquery';
 import './styles/login.css'
 
 export default function LoginComponent(){
@@ -45,12 +46,45 @@ export default function LoginComponent(){
     }
 }
 
+useEffect(() => {
+    $('input').blur(function() {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    var $ripples = $('.ripples');
+
+    $ripples.on('click.Ripples', function(e) {
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+        $(this).removeClass('is-active');
+    });
+}, []);
 
     return (
+        
         <div className="Login">
+            <SuccessMessageComponent/>
+            <ErrorMessageComponent/>
             <div className="LoginForm">
-                <SuccessMessageComponent/>
-                <ErrorMessageComponent/>
+                
                 <div className="group">
                     <input type="text" name="username" onChange={handleUsernameChange} />
                     <span className="highlight"></span><span className="bar"></span>
