5d59f20ba5b2b76fc94a7ea5563de36b6cdd0fd9
animesh <sethanimesh@hotmail.com>
2024-08-07 11:08:02 +0530

    routing to login component #1

--- src/components/CreditCardApp.jsx
+++ src/components/CreditCardApp.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginComponent from "./LoginComponent";
 
 export default function CreditCardApp(){
-
     return (
         <>
-            <LoginComponent/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginComponent/>}/>
+                    <Route path="/login" element={<LoginComponent/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
